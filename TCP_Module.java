261ccee9273977918a5c468b290d9462380cba43
dingyao1112 <50368625+dingyao1112@users.noreply.github.com>
2019-05-22 16:23:56 +0800

    Add files via upload

--- TCP_Module.java
+++ TCP_Module.java
@@ -41,12 +41,7 @@ public class TCP_Module {
 	private static int storage_key=Tool.get_mysql_datalen();//数据编号的key,从数据库中读取
 	private static int storage_datalen;
 	private static byte[] storage_data;
-	private static byte[] froward_data;//转发数据
 	private static byte[] IO_data;
-	private static int froward_datalen;
-	private static int flag=0;//开启线程变量
-	private static int closeid=0;
-	private static boolean IO_response=false;//判断是IO转发还是数据转发
 	private static Map<Integer,Integer> relativelyid;//相对ID
 	private static Map<Byte,OutputStream> send_data_output;
 	//num用来记录不同客户端
@@ -55,7 +50,6 @@ public class TCP_Module {
 		try {
 			System.out.println(storage_key);
 			num=0;
-			froward_data=new byte[200];
 			IO_data=new byte[2];
 			metoyou=new HashMap<Integer,Byte>();
 			youtome=new HashMap<Byte,Integer>();
@@ -74,7 +68,7 @@ public class TCP_Module {
 			}
 			server=new ServerSocket(PORT);
 			ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
-//			cachedThreadPool.execute(new Threadprintf());
+			cachedThreadPool.execute(new Threadprintf());
 			while(true) {
 				//接收客户端，此程序为阻塞式的
 				Socket client=server.accept();
@@ -111,7 +105,7 @@ public class TCP_Module {
 					}
 				}
 				try {
-					Thread.sleep(3000);
+					Thread.sleep(5000);
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -142,16 +136,19 @@ public class TCP_Module {
 				byte[] comm= {metoyou.get(0x5A),metoyou.get(0x80),metoyou.get(0x03)};
 				byte[] storage_data_success= {metoyou.get(0x5A),metoyou.get(0x80),metoyou.get(0x06)};
 				byte[] storage_data_failure= {metoyou.get(0x5A),metoyou.get(0x80),metoyou.get(0x07)};
+				byte[] senddata_error= {metoyou.get(0x5A),metoyou.get(0x80),metoyou.get(0x04)};
 				byte[] read_data_failure= {metoyou.get(0x5A),metoyou.get(0x80),metoyou.get(0x08)};
+				byte[] no_bind_id= {metoyou.get(0x5A),metoyou.get(0x80),metoyou.get(0x03)};
+				byte[] repeatid= {metoyou.get(0x5A),metoyou.get(0x81),metoyou.get(0x05)};
+				byte[] usableid= {metoyou.get(0x5A),metoyou.get(0x81),metoyou.get(0x00)};
 				byte[] buf=new byte[1024];
+				boolean bind_id=false;
 				int len=0;
 				System.out.println("IP:"+socket.getLocalAddress().getHostAddress()+"  ");
 				//获取客户端连接时间
 				Date day=new Date();    
 				SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); 
 				System.out.print(df.format(day)+"*********");  
-//				//开启监听flag线程
-				//new Thread(new monitorflag(os,num)).start();
 				while((len=is.read(buf))!=-1) {
 					//通信测试
 					if(buf[0]==metoyou.get(0x5A)&&buf[1]==metoyou.get(0x00)&&
@@ -162,30 +159,33 @@ public class TCP_Module {
 					//id绑定
 					else if(buf[0]==metoyou.get(0x5A)&&buf[1]==metoyou.get(0x01)&&len==5) {
 						//绑定相对id
-						bin_relativelyid(buf[4], os);
-						send_data_output.put(buf[4], os);
-						//绑定绝对Id
-						byte[] arrayabsoluteid= {buf[2],buf[3]};
-						absoluteid.put(num, arrayabsoluteid);
-						
+						if(relativelyid.containsValue(youtome.get(buf[4]))) {
+							os.write(repeatid);
+						}else {
+							relativelyid.put(num, youtome.get(buf[4]));
+							os.write(usableid);
+							send_data_output.put(buf[4], os);
+							//绑定绝对Id
+							byte[] arrayabsoluteid= {buf[2],buf[3]};
+							absoluteid.put(num, arrayabsoluteid);
+							bind_id=true;//绑定ID成功
+						}
 					}
 					//数据转发
 					else if(buf[0]==metoyou.get(0x5A)&&buf[1]==metoyou.get(0x02)&&len>3) {
-						//存储数据位
-//						froward_datalen=len;
-//						froward_data[0]=metoyou.get(0x5A);
-//						froward_data[1]=metoyou.get(0x00);
-//						froward_data[2]=metoyou.get(relativelyid.get(num));
-//						for(int i=3;i<len;i++) {
-//							froward_data[i]=buf[i];
-//						}
-						send_data_output.get(buf[2]).write(buf, 3, len);
-						//flag用来监控服务器給哪个Id发送数据
-//						System.out.println("*******"+youtome.get(buf[2])+"*******");
-						int ln=getflag(buf[2]);
-						IO_response=false;
-						flag=ln;
-//						System.out.println("给"+relativelyid.get(flag)+"发送信息");
+						if(relativelyid.containsValue(youtome.get(buf[2]))) {
+							if(bind_id) {
+								byte id=buf[2];
+								buf[1]=metoyou.get(0x00);
+								buf[2]=metoyou.get(relativelyid.get(num));
+								send_data_output.get(id).write(buf,0, len);//转发数据
+							}
+							else {
+								os.write(no_bind_id);//设备没有绑定ID
+							}
+						}else {
+							os.write(senddata_error);//ID不在线
+						}
 					}
 					//数据存储
 					else if(buf[0]==metoyou.get(0x5A)&&buf[1]==metoyou.get(0x04)&&len>4) {
@@ -225,18 +225,14 @@ public class TCP_Module {
 					//IO转发
 					else if(buf[0]==metoyou.get(0x5A)&&buf[1]==metoyou.get(0x03)&&
 							len==6&&(buf[5]==metoyou.get(0x01)||buf[5]==metoyou.get(0x00))) {
-						IO_response=true;
 						IO_data[0]=buf[3];
 						IO_data[1]=buf[4];
 					}
 					}
-				closeid=num;
-				socket.close();//关闭socket
 				//移除ID
 			} catch (IOException e) {
 				e.printStackTrace();
 			}finally {
-				closeid=num;
 				try {
 					socket.close();
 				} catch (IOException e) {
@@ -336,99 +332,5 @@ public class TCP_Module {
 			return 0;
 		}
 		
-		//*******************绑定相对id********************//
-		public void bin_relativelyid(byte a,OutputStream os) throws IOException {
-			byte[] repeatid= {metoyou.get(0x5A),metoyou.get(0x81),metoyou.get(0x05)};
-			byte[] usableid= {metoyou.get(0x5A),metoyou.get(0x81),metoyou.get(0x00)};
-				//绑定相对id
-				//判断Id是否重复
-				System.out.println("id绑定");
-				Set<Integer> keyset=relativelyid.keySet();
-				Iterator<Integer> it=keyset.iterator();
-				int exp=0;
-				//遍历relativelyid中的value
-				while(it.hasNext()) {
-					int key=it.next();
-					if(metoyou.get(relativelyid.get(key))==a) {
-						os.write(repeatid);//id重复发送数据给客户端表示绑定失败
-						exp=1;
-						break;
-					}
-				}
-				if(exp==0) {
-					relativelyid.put(num,youtome.get(a));
-					os.write(usableid);//id绑定成功发送数据给客户端
-				}
-					
-			
-		}
-		
 	}
-	
-	//*************************监控接受转发数据***********************************//
-	private static class monitorflag implements Runnable{
-		
-		private OutputStream os1;
-		private int num1;
-		public monitorflag(OutputStream os,int num) {
-			os1=os;
-			num1=num;
-		}
-		
-		public void run() {
-			System.out.println(num+"无限监听线程开启");
-			while(true) {
-				try {
-					//疑问，为什么这里要sleep否则客户端监听线程只能开一个
-					Thread.sleep(1000);
-//					System.out.println(num1+"无限线程开启");
-				} catch (InterruptedException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-				if(flag==num1){
-					flag=0;
-					try {
-						if(IO_response) {
-							os1.write(IO_data);	
-							}else {
-							os1.write(froward_data,0,froward_datalen);
-						}
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-					try {
-						String data=new String(froward_data, 3, froward_datalen, "utf-8");
-						Date day=new Date();    
-						SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); 
-						System.out.print(df.format(day)+"*********");  
-						System.out.println("转发数据为:"+data);
-					} catch (UnsupportedEncodingException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-				if(closeid==num1) {
-					Date day=new Date();    
-					SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); 
-					System.out.println(df.format(day)+"*********"+num1+"无限线程关闭");
-					closeid=0;
-					try {
-						os1.close();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					break;
-				}
-			}
-		}
-		
-	}
-
-	public static boolean Select_User(Socket socket) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
 }
